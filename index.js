f7fb483c9c933fb94b730ec280d307b5793d7998
Ana <amarcosestevez@gmail.com>
2022-07-28 13:59:06 +0200

    refactory the function to add the open-active class when the opening transition  has finished

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const panels = document.querySelectorAll('.panel');
 
 function handleClick(ev){
 const currentPanel = ev.currentTarget;
-console.log(ev.target)
-console.log(ev.currentTarget)
 currentPanel.classList.toggle('open');
-currentPanel.classList.toggle('open-active')
+
 }
+
+function handleText(ev){
+    const property = ev.propertyName;
+    const currentPanel = ev.currentTarget;
+    if(property.includes('flex')){
+        currentPanel.classList.toggle('open-active')
+    }
+}
+
 for (let i=0; i< panels.length; i++){
     const panel = panels[i];
-    panel.addEventListener('click', handleClick)
+    panel.addEventListener('click', handleClick);
+    panel.addEventListener('transitionend', handleText)
 }
